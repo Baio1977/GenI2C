81c825e275db1c8ab782268087f9f75439c86586
Baio77 <59058746+Baio1977@users.noreply.github.com>
2024-01-08 00:44:57 +0100

    Update GenReadme.swift

--- GenI2C/GenReadme.swift
+++ GenI2C/GenReadme.swift
@@ -14,7 +14,7 @@ func GenReadme (Rename: String) {
     Readme += NSLocalizedString("1. Opencore Bootloader : Put VoodooI2C and the Satellite into EFI/OC/Kext , generate rename(s) with (https://github.com/corpnewt/ACPIRename) and insert in config.plist\n", comment: "")
     Readme += NSLocalizedString("2. Clover Bootloader : Put VoodooI2C and the Satellite into Clover/Kexts/Other , place SSDT aml files into Clover/ACPI/patched add insert rename(s) in config.plist\n", comment: "")
     Readme += Rename
-    Readme += NSLocalizedString("3.\n\n3 If you have problems here: https://github.com/Baio1977/GenI2C\n\n", comment: "")
+    Readme += NSLocalizedString("3.\n\n If you have problems here: https://github.com/Baio1977/GenI2C\n\n", comment: "")
     
     let path:String = FolderPath + "/Readme.txt"
     try! FileManager.default.createDirectory(atPath: FolderPath, withIntermediateDirectories: true, attributes: nil)
